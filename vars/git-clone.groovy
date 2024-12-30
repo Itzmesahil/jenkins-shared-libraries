98326d61dd4d9939b6e3a87b147b9ac5fa020405
Sahil Shaikh <119860538+Itzmesahil@users.noreply.github.com>
2024-12-31 01:45:15 +0530

    Update git-clone.groovy

--- vars/git-clone.groovy
+++ vars/git-clone.groovy
@@ -1,16 +1,19 @@
+def call(String url, String branch) {
+    echo "Cloning the code from repository: ${url} and branch: ${branch}"
+    
+    // Checkout code from the Git repository
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[url: url]]
+    ])
+    
+    echo "Code cloned successfully"
+}
+
 // def call(String url, String branch){
 //   echo "This is cloning the code" 
 //   git url: "${url}", branch: "${branch}"
 //   echo "Code clonning Successfully"
 // }
 
-def call(String url, String branch) {
-    echo "This is cloning the code"
-    
-    // Using the 'git' step to clone the repository
-    checkout([$class: 'GitSCM', 
-              branches: [[name: "*/${branch}"]], 
-              userRemoteConfigs: [[url: url]]])
-    
-    echo "Code cloning successfully"
-}
