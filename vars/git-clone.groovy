14b23dc64f8577aa2a455c82f141aaf18d1fc034
Sahil Shaikh <119860538+Itzmesahil@users.noreply.github.com>
2024-12-31 01:54:38 +0530

    Update git-clone.groovy

--- vars/git-clone.groovy
+++ vars/git-clone.groovy
@@ -1,3 +1,7 @@
-def call(String GitUrl, String GitBranch){
-  git url: "${GitUrl}", branch: "${GitBranch}"
+def call(String GitUrl, String GitBranch) {
+    echo "Cloning repository from ${GitUrl}, branch: ${GitBranch}"
+    git url: GitUrl, branch: GitBranch
 }
+// def call(String GitUrl, String GitBranch){
+//   git url: "${GitUrl}", branch: "${GitBranch}"
+// }
