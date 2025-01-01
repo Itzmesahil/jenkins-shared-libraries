8da95c7484beeb91d89c39ec4d323a53d9175664
Sahil Shaikh <119860538+Itzmesahil@users.noreply.github.com>
2025-01-01 18:45:45 +0530

    Update docker_clean.groovy

--- vars/docker_clean.groovy
+++ vars/docker_clean.groovy
@@ -2,4 +2,6 @@ def call(){
   echo "Cleaning up stopped containers and unused images"
   sh 'docker container prune -f'
   sh 'docker image prune -a -f'
+  echo "Cleaning Jenkins workspace..."
+  deleteDir() 
 }
